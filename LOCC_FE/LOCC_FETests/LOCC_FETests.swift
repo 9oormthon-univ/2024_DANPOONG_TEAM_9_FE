06873a8a6cce4e8b3506612f89b59314ef6274cf
sunhanmoo <ghdms1013@naver.com>
2024-11-18 17:40:11 +0900

    Begin : 프로젝트 세팅

---
+++ LOCC_FE/LOCC_FETests/LOCC_FETests.swift
@@ -0,0 +1,17 @@
+//
+//  LOCC_FETests.swift
+//  LOCC_FETests
+//
+//  Created by 성호은 on 11/18/24.
+//
+
+import Testing
+@testable import LOCC_FE
+
+struct LOCC_FETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
