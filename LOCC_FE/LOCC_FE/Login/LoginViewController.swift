691c5dce3f2e5c1f920cf0fbcd78a5d8cee99c4b
sunhanmoo <ghdms1013@naver.com>
2024-11-19 18:11:46 +0900

    Add : 스플래시뷰 아이콘 추가 (#9)

--- LOCC_FE/LOCC_FE/Login/LoginViewController.swift
+++ LOCC_FE/LOCC_FE/Login/LoginViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // button
+    @IBOutlet weak var logo: UIButton!
+    @IBOutlet weak var kakaoLoginBtn: UIButton!
+    
+    // label
+    @IBOutlet weak var introduceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupUI() {
+        let introduceText = "로컬 크리에이터들을\n한 손에서 발견!"
+        let attributedString = NSMutableAttributedString(string: introduceText)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // 특정 범위의 텍스트에 글꼴을 변경
+        let range = (introduceText as NSString).range(of: "로컬 크리에이터")
+        let customFont = UIFont(name: "Pretendard-Bold", size: 21) ?? UIFont.systemFont(ofSize: 21) // 원하는 글꼴 및 크기 설정
+        attributedString.addAttribute(.font, value: customFont, range: range)
+
+        introduceLabel.attributedText = attributedString
     }
-    */
 
 }
---
+++ LOCC_FE/LOCC_FE/Splash/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  LOCC_FE
+//
+//  Created by 성호은 on 11/19/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
