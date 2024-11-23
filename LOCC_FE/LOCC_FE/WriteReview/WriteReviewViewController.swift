1a65747f12192bf7922bf156d4989a9b23d0f9fb
sunhanmoo <ghdms1013@naver.com>
2024-11-23 10:49:55 +0900

    Add : 코스모스 라이브러리 추가 (#25)

---
+++ LOCC_FE/LOCC_FE/WriteReview/WriteReviewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriteReviewViewController.swift
+//  LOCC_FE
+//
+//  Created by 성호은 on 11/23/24.
+//
+
+import UIKit
+
+class WriteReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
